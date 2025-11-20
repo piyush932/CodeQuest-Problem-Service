259690cb74a65a4cdcc8fb61221f0c201cb8b9cd
Piyush <b521044@iiit-bh.ac.in>
2025-11-20 22:07:15 +0530

    Docs: Added documentation

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -4,6 +4,7 @@ const problemRouter = require("./problems.routes");
 
 const v1Router = express.Router();
 
+// If any request comes and route continues with /problems, we map it to problemRouter
 v1Router.use('/problems',problemRouter);
 
 module.exports = v1Router;
