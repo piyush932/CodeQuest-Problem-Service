c75f3f97a8a1102f30976fc477e7816d0e01022e
Piyush <b521044@iiit-bh.ac.in>
2025-11-29 18:55:30 +0530

    Feat: Added error handler middleware with basic error classes

---
+++ src/errors/notImplemented.error.js
@@ -0,0 +1,11 @@
+const { StatusCodes } = require("http-status-codes");
+const BaseError = require("./base.error");
+
+class NotImplemented extends BaseError {
+    constructor(methodName){
+        super("NotImplemented", StatusCodes.NOT_IMPLEMENTED, `${methodName} Not Implemented`, {});
+    }
+}
+
+module.exports = NotImplemented;
+
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const {PORT} = require("./config/server-config");
 const apiRouter = require("./routes");
+const errorHandler = require("./utils/errorHandler");
 
 const app = express();
 
@@ -15,6 +16,9 @@ app.use('/api',apiRouter);
 app.get('/ping',(req,res)=>{
     return res.json({message: 'Problem Service is alive'});
 })
+
+// last middleware if any error comes
+app.use(errorHandler);
 
 app.listen((PORT),()=>{
     console.log(`Server started at PORT: ${PORT}`);
