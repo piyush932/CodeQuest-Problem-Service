efb9d97abae9246efd889ef56c0d09c7490f8b2d
Piyush <b521044@iiit-bh.ac.in>
2025-12-20 19:06:58 +0530

    Feat: Completed create problem api

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -1,19 +1,28 @@
-const {StatusCodes} = require("http-status-codes");
+const { StatusCodes } = require("http-status-codes");
 const BaseError = require("../errors/base.error");
 const BadRequest = require("../errors/badrequest.error");
 const NotImplemented = require("../errors/notImplemented.error");
+const ProblemRepository = require("../repositories/problem.repository");
+const ProblemService = require("../services/problem.service");
+
+const problemService = new ProblemService(new ProblemRepository());
 
 function pingProblemController(req, res) {
   return res.json({ message: "Problem controller is up" });
 }
 
-function addProblem(req, res, next) {
+async function addProblem(req, res, next) {
   try {
-    throw new BadRequest('Problem Name', {missing: ["Problem Name"]});
-    // nothing implemented
-        // throw new NotImplemented('Add Problem');
+    console.log("Incoming request", req.body);
+    const newproblem = await problemService.createProblem(req.body);
+    return res.status(StatusCodes.CREATED).json({
+      success: true,
+      message: "Successfully created a new problem",
+      error: {},
+      data: newproblem,
+    });
   } catch (error) {
-    next(error)
+    next(error);
   }
 }
 
